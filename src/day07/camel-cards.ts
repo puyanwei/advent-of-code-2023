904b2fbc5ba95dd346a42580741a04f6ec7eaf52
Puyan Wei <puyanwei@gmail.com>
2024-03-16 10:55:30 +0000

    day7 planning

---
+++ src/day07/camel-cards.ts
@@ -0,0 +1,19 @@
+export function camelCards() {
+  return 1
+}
+
+/*
+Order of tasks
+1. Arrange data into workable format;
+array of;
+  hand: array of cards,
+  bid: number
+  ranking? : number // might help with part 2
+
+2. Create a system for ranking the cards, could create my own points system?
+   Logic should include counting duplicate cards in the hand
+   Draws need another logic compare the strength of each card by index
+
+3. Reorder hands by strength
+4. Sum up with the bid
+*/
--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import { cubeConundrum1 } from "./day02/cube-conundrum-1"
 import { cubeConundrum2 } from "./day02/cube-conundrum-2"
 import { gearRatios1 } from "./day03/gear-ratios-1"
 import { scratchcards1 } from "./day04/scratchcards-1"
+import { camelCards } from "./day07/camel-cards"
 
 const intro = "Hello, Mr Puyan Wei. Welcome to Advent of Code 2023!"
 
@@ -19,4 +20,5 @@ console.log(intro)
 //   `Day 2, part 2 - The sum of all of the possible games with the fewest number of cubes is ${cubeConundrum2()}`
 // )
 // console.log(`Day 3, part 1 - The sum of all of the parts is ${gearRatios1()}`)
-console.log(`Day 4, part 1 - The points from the scratchcards are ${scratchcards1()}`)
+// console.log(`Day 4, part 1 - The points from the scratchcards are ${scratchcards1()}`)
+console.log(`The total winnings are ${camelCards()}`)
