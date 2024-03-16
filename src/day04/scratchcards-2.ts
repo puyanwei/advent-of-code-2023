2931991336ca16aac0d99c40d8e45121888dc54b
Puyan Wei <puyanwei@gmail.com>
2024-03-16 10:42:59 +0000

    day 4 refactoring

--- src/day04/scratchcards-2.ts
+++ src/day04/scratchcards-2.ts
@@ -9,35 +9,50 @@ export function addDuplicateCount(scratchcards: Scratchcard[]): ScratchcardInfo[
 }
 
 export function bonusScratchcards(number: number): number[] {
-  return Array(number).fill(1)
+  if (number === 0) return []
+  let array = []
+  for (let index = 0; index < number; index++) {
+    array.push(1)
+  }
+  return array
 }
 
+// TODO: I think I need to add the arrays but skip the first element as it never adds a ticket to itself
 export function addArrays(arr1: number[], arr2: number[]): number[] {
   const len1 = arr1.length
   const len2 = arr2.length
-
   // Find the maximum length between the two arrays
   const maxLen = Math.max(len1, len2)
 
   // Pad the shorter array with zeros
   arr1.push(...Array(maxLen - len1).fill(0))
   arr2.push(...Array(maxLen - len2).fill(0))
-
   // Add the elements of the two arrays
-  const result = arr1.map((value, index) => value + arr2[index])
+  const result = arr1.map((value, index) => {
+    console.log(value, arr2[index - 1], arr1, arr2)
+    if (index === 0) return value
+    return value + arr2[index - 1]
+  })
 
   return result
 }
 
 export function countScratchcards(scratchcardInfo: ScratchcardInfo[]): number {
   const scratchcards = scratchcardInfo.reduce((prev, current, index) => {
-    // Keep track of future scratchcards and calculate current one
     let ticketCount = [1]
     const duplicates = duplicateElements(current.playerNumbers, current.winningNumbers)
-    const extraScratchcards = (prev += duplicates + ticketCount[0])
-    ticketCount = addArrays(ticketCount, bonusScratchcards(duplicates))
-    ticketCount.shift()
+    const arrayOfOnes = bonusScratchcards(duplicates) // Creates an array of [n * 1's] to add to tickeCount array
+    const newTicketCount = addArrays(ticketCount, arrayOfOnes)
+    console.log({ newTicketCount })
+    const extraScratchcards = index === 0 ? (prev += 1) : (prev += newTicketCount[0])
+    ticketCount = newTicketCount
+    if (duplicates === 0) ticketCount = [1]
     return extraScratchcards
   }, 0)
   return scratchcards
 }
+/*
+Card 1 - [1,1,1,1,1] // 1
+Card 2 - [1,2,2,1] // 1 + 2
+Card 3 - [3,2] // 
+*/
--- src/day04/scratchcards.test.ts
+++ src/day04/scratchcards.test.ts
@@ -60,20 +60,129 @@ describe("Day 4 Part 2 - Scratchcards", () => {
   test("bonusScratchcards()", () => {
     expect(bonusScratchcards(2)).toEqual([1, 1])
     expect(bonusScratchcards(6)).toEqual([1, 1, 1, 1, 1, 1])
+    expect(bonusScratchcards(3)).toEqual([1, 1, 1])
   })
 
-  test("addArrays", () => {
-    expect(addArrays([1, 2, 3, 4], [5, 6, 7])).toEqual([6, 8, 10, 4])
+  test.only("addArrays", () => {
+    expect(addArrays([1, 2, 3, 4], [5, 6, 7])).toEqual([1, 7, 9, 11])
+    expect(addArrays([1, 1], [3, 2])).toEqual([1, 4, 2])
+    // expect(addArrays([], [3, 2])).toEqual([3, 2])
+    // expect(addArrays([3, 3], [])).toEqual([3, 3])
   })
 
-  test.only("countScratchcards()", () => {
-    const scratchcardInfo: ScratchcardInfo[] = [
+  describe("countScratchcards()", () => {
+    test("One ticket", () => {
       {
-        winningNumbers: [41, 48, 83, 86, 17],
-        playerNumbers: [83, 86, 6, 31, 17, 9, 48, 53],
-        duplicates: 4,
-      },
-    ]
-    expect(countScratchcards(scratchcardInfo)).toEqual(5)
+        const scratchcardInfo: ScratchcardInfo[] = [
+          {
+            winningNumbers: [41, 48, 83, 86, 17],
+            playerNumbers: [83, 86, 6, 31, 17, 9, 48, 53],
+            duplicates: 4,
+          },
+        ]
+        expect(countScratchcards(scratchcardInfo)).toEqual(1)
+      }
+    })
+    test("Two tickets", () => {
+      const scratchcardInfo: ScratchcardInfo[] = [
+        {
+          winningNumbers: [41, 48, 83, 86, 17],
+          playerNumbers: [83, 86, 6, 31, 17, 9, 48, 53],
+          duplicates: 4,
+        },
+        {
+          winningNumbers: [13, 32, 20, 16, 61],
+          playerNumbers: [61, 30, 68, 82, 17, 32, 24, 19],
+          duplicates: 2,
+        },
+      ]
+      expect(countScratchcards(scratchcardInfo)).toEqual(3)
+    })
+    test("Three tickets", () => {
+      const scratchcardInfo: ScratchcardInfo[] = [
+        {
+          winningNumbers: [41, 48, 83, 86, 17],
+          playerNumbers: [83, 86, 6, 31, 17, 9, 48, 53],
+          duplicates: 4,
+        },
+        {
+          winningNumbers: [13, 32, 20, 16, 61],
+          playerNumbers: [61, 30, 68, 82, 17, 32, 24, 19],
+          duplicates: 2,
+        },
+        {
+          winningNumbers: [1, 21, 53, 59, 44],
+          playerNumbers: [69, 82, 63, 72, 16, 21, 14, 1],
+          duplicates: 2,
+        },
+      ]
+      expect(countScratchcards(scratchcardInfo)).toEqual(7)
+    })
+    test("Four tickets", () => {
+      const scratchcardInfo: ScratchcardInfo[] = [
+        {
+          winningNumbers: [41, 48, 83, 86, 17],
+          playerNumbers: [83, 86, 6, 31, 17, 9, 48, 53],
+          duplicates: 4,
+        },
+        {
+          winningNumbers: [13, 32, 20, 16, 61],
+          playerNumbers: [61, 30, 68, 82, 17, 32, 24, 19],
+          duplicates: 2,
+        },
+        {
+          winningNumbers: [1, 21, 53, 59, 44],
+          playerNumbers: [69, 82, 63, 72, 16, 21, 14, 1],
+          duplicates: 2,
+        },
+        {
+          winningNumbers: [1, 21, 53, 59, 44],
+          playerNumbers: [69, 82, 63, 72, 16, 21, 14, 1],
+          duplicates: 2,
+        },
+      ]
+      expect(countScratchcards(scratchcardInfo)).toEqual(2)
+    })
+    // {
+    //   const scratchcardInfo: ScratchcardInfo[] = [
+    //     {
+    //       winningNumbers: [41, 48, 83, 86, 17],
+    //       playerNumbers: [83, 86, 6, 31, 17, 9, 48, 53],
+    //       duplicates: 4,
+    //     },
+    //     {
+    //       winningNumbers: [13, 32, 20, 16, 61],
+    //       playerNumbers: [61, 30, 68, 82, 17, 32, 24, 19],
+    //       duplicates: 2,
+    //     },
+    //     {
+    //       winningNumbers: [1, 21, 53, 59, 44],
+    //       playerNumbers: [69, 82, 63, 72, 16, 21, 14, 1],
+    //       duplicates: 2,
+    //     },
+    //     {
+    //       winningNumbers: [1, 21, 53, 59, 44],
+    //       playerNumbers: [69, 82, 63, 72, 16, 21, 14, 1],
+    //       duplicates: 2,
+    //     },
+    //     {
+    //       winningNumbers: [41, 92, 73, 84, 69],
+    //       playerNumbers: [59, 84, 76, 51, 58, 5, 54, 83],
+    //       duplicates: 1,
+    //     },
+    //     {
+    //       winningNumbers: [87, 83, 26, 28, 32],
+    //       playerNumbers: [88, 30, 70, 12, 93, 22, 82, 36],
+    //       duplicates: 0,
+    //     },
+    //     {
+    //       winningNumbers: [31, 18, 13, 56, 72],
+    //       playerNumbers: [74, 77, 10, 23, 35, 67, 36, 11],
+    //       duplicates: 0,
+    //     },
+    //   ]
+
+    //   expect(countScratchcards(scratchcardInfo)).toEqual(30)
+    // }
   })
 })
